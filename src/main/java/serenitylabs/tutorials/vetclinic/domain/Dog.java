e065de247fa7bdfafb26d04a2289c95def2139aa
Pierre Kanoha <pierre.kanoha@gmail.com>
2021-07-06 21:38:58 +0200

    builders in action

--- src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
+++ src/main/java/serenitylabs/tutorials/vetclinic/domain/Dog.java
@@ -1,33 +1,70 @@
 package serenitylabs.tutorials.vetclinic.domain;
 
+import java.security.cert.CertPathBuilder;
 import java.time.LocalDateTime;
 
 public class Dog {
-    private String name;
-    private String breed;
-    private LocalDateTime dateOfBirth;
+    private final String name;
+    private final String breed;
+    private final LocalDateTime dateOfBirth;
+    private final String favouriteFood;
 
-    public void setName(String name) {
+    //variables become immutable because name, breed, dateOfBirth are assigned into the constructor
+
+    /*public Dog(String name, String breed, LocalDateTime dateOfBirth) {
         this.name = name;
+        this.breed = breed;
+        this.dateOfBirth = dateOfBirth;
+        this.favouriteFood = null;
+    }*/
+ //or like this
+    public Dog(String name, String breed, LocalDateTime dateOfBirth) {
+        this(name, breed, dateOfBirth, null);
+    }
+
+    public Dog(String name, String breed, LocalDateTime dateOfBirth, String favouriteFood) {
+        this.name = name;
+        this.breed = breed;
+        this.dateOfBirth = dateOfBirth;
+        this.favouriteFood = favouriteFood;
+    }
+
+    public static DogBuilder called(String name) {
+        return new DogBuilder(name);
     }
 
     public String getName() {
         return name;
     }
 
-    public void setBreed(String breed) {
-        this.breed = breed;
-    }
-
     public String getBreed() {
         return breed;
     }
 
-    public void setDateOfBirth(LocalDateTime dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
     public LocalDateTime getDateOfBirth() {
         return dateOfBirth;
+    }
+
+    public String getFavouriteFood() {
+        return favouriteFood;
+    }
+
+    public static class DogBuilder {
+        private String name;
+        private String breed;
+        private LocalDateTime birthday;
+
+        public DogBuilder(String name) {
+            this.name = name;
+        }
+
+        public DogBuilder ofBreed(String breed) {
+            this.breed = breed;
+            return this;
+        }
+
+        public Dog bornOn(LocalDateTime birthday) {
+            return new Dog(name, breed, birthday);
+        }
     }
 }
--- src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenCreatingANewDog.java
+++ src/test/java/serenitylabs/tutorials/vetclinic/domain/WhenCreatingANewDog.java
@@ -8,15 +8,23 @@ import java.time.LocalDateTime;
 public class WhenCreatingANewDog {
     @Test
     public void it_should_have_a_name() throws Exception {
-        Dog fido = new Dog();
         LocalDateTime birthday = LocalDateTime.now();
 
-        fido.setName("Fido");
-        fido.setBreed("Poodle");
-        fido.setDateOfBirth(birthday);
+        Dog fido = Dog.called("Fido")
+                      .ofBreed("Poodle")
+                      .bornOn(birthday);
 
         Assert.assertEquals("Fido", fido.getName());
         Assert.assertEquals("Poodle", fido.getBreed());
         Assert.assertEquals(birthday, fido.getDateOfBirth());
+    }
+
+    @Test
+    public void it_should_have_an_optional_favourite_food() throws Exception {
+        LocalDateTime birthday = LocalDateTime.now();
+
+        Dog fido = new Dog("Fido", "Poodle", birthday, "Pizza");
+
+        Assert.assertEquals("Pizza", fido.getFavouriteFood());
     }
 }
